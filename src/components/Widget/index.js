ba4dcc7e02dfea7af49b5a8b25b02769a0b79716
dakavisa <dakavisa623@gmail.com>
2019-12-03 12:45:04 -0600

    Fixed multiple browsers with same session

--- src/components/Widget/index.js
+++ src/components/Widget/index.js
@@ -1,22 +1,28 @@
-import React, { Component } from 'react';
-import { compose } from 'redux';
-import { connect } from 'react-redux';
+import React, {Component} from 'react';
+import {compose} from 'redux';
+import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
 import Nes from 'nes';
+import Guid from 'guid';
 
-import { toggleChat, addUserMessage, addResponseMessage, toggleMsgLoader, setQuickButtons } from '@actions';
+import {
+  toggleChat,
+  addUserMessage,
+  addResponseMessage,
+  toggleMsgLoader,
+  setQuickButtons,
+} from '@actions';
 
 import WidgetLayout from './layout';
 
 class Widget extends Component {
-
   state = {
     client: null,
     socketClientConnected: false,
     waitingForMessage: false,
   };
 
-  componentWillMount(){
+  componentWillMount() {
     if (!this.state.socketClientConnected) {
       const client = new Nes.Client(this.props.socketUrl);
       client.onConnect = () => {
@@ -27,28 +33,37 @@ class Widget extends Component {
 
         const handler = response => {
           if (response) {
-            if (this.state.waitingForMessage){
+            if (this.state.waitingForMessage) {
               this.setState({
-                waitingForMessage: false
+                waitingForMessage: false,
               });
-              this.props.dispatch(toggleMsgLoader());              
+              this.props.dispatch(toggleMsgLoader());
             }
             this.props.dispatch(addResponseMessage(response.textResponse));
             if (response.quickResponses) {
-              this.props.dispatch(setQuickButtons(response.quickResponses.map((quickResponse) => {
-                return {
-                  value: quickResponse,
-                  label: quickResponse
-                }
-              })));
+              this.props.dispatch(
+                setQuickButtons(
+                  response.quickResponses.map(quickResponse => {
+                    return {
+                      value: quickResponse,
+                      label: quickResponse,
+                    };
+                  }),
+                ),
+              );
             }
           }
         };
-
-        client.subscribe(
-          this.props.socketPath,
-          handler,
-        );
+        const storedSessionId = localStorage.getItem('sessionId');
+        let sessionId;
+        if (storedSessionId) {
+          sessionId = storedSessionId;
+        } else {
+          const newSessionId = Guid.create().toString();
+          sessionId = newSessionId;
+          localStorage.setItem('sessionId', newSessionId);
+        }
+        client.subscribe(this.props.socketPath + '/' + sessionId, handler);
       };
       client.connect();
     }
@@ -62,30 +77,30 @@ class Widget extends Component {
 
   toggleConversation = () => {
     this.props.dispatch(toggleChat());
-  }
+  };
 
-  handleMessageSubmit = (event) => {
+  handleMessageSubmit = event => {
     event.preventDefault();
     const userInput = event.target.message.value;
     if (userInput.trim()) {
       this.getResponseFromMessage(userInput);
     }
     event.target.message.value = '';
-  }
+  };
 
   handleQuickButtonClicked = (event, value) => {
     event.preventDefault();
     this.getResponseFromMessage(value);
     this.props.dispatch(setQuickButtons([]));
-  }
+  };
 
-  getResponseFromMessage = (text) => {
+  getResponseFromMessage = text => {
     this.props.dispatch(addUserMessage(text, this.props.converseUrl));
     this.setState({
-      waitingForMessage: true
+      waitingForMessage: true,
     });
     this.props.dispatch(toggleMsgLoader());
-  }
+  };
 
   render() {
     return (
@@ -126,6 +141,4 @@ Widget.propTypes = {
 
 const withConnect = connect();
 
-export default compose(
-  withConnect
-)(Widget);
+export default compose(withConnect)(Widget);
